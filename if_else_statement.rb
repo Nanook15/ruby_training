5c698d6f334f968596e4b95b13a3a460759de261
Nanook15 <nanook.eth@protonmail.com>
2021-12-30 15:52:48 +0000

    Some Practice Files

---
+++ if_else_statement.rb
@@ -0,0 +1,15 @@
+print "Gimme a number and Ill suck youse dick "
+  num = gets.to_i
+
+if num == 20
+  puts "You da hot bitch"
+elsif num == 27
+  puts "Oh my..."
+elsif num == 50
+  puts "You so ripe for it"
+elsif num >= 18
+  puts "Sweet"
+else num <= 18
+  puts "get outta here you sick fuck"
+end
+
